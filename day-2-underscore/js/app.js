ade1dc9a77094465e307655244ace2f7286ff1de
kannan <kannan.90@gmail.com>
2014-06-04 01:56:19 +0800

    wrapping up underscore

--- day-2-underscore/js/app.js
+++ day-2-underscore/js/app.js
@@ -10,6 +10,7 @@ var phones;
 function loadPhones() {
   $.getJSON("json/phones/phones.json", function(obj) {
     phones = obj;
+
     //keep track of async requests
     requests = [];
     _.each(obj,function(phone) {
@@ -32,12 +33,10 @@ function loadPhones() {
 
 //function to append header and string to a root
 function appendToElement(el,header,content) {
-  $("<h3></h3>")
-    .html(header)
-    .appendTo(el);
-  $("<p></p>")
-    .html(content)
-    .appendTo(el);
+
+  //templates!
+  var sectionString = _.template("<h3><%= header %></h3><p><%= content %></p>");
+  el.append(sectionString({header:header,content:content}));
 }
 
 // most of the app
@@ -55,10 +54,41 @@ function summarize() {
   mapResult = _.map(phones,function(phone) {
     return "<p>"+phone.name + " - " + phone.specs.android.os+"</p>";
   });
-  appendToElement(root,"map",mapResult.join(""));
+  appendToElement(root,"map - os versions",mapResult.join(""));
 
-  
+  //reduce
+  redResult = 
+    _.reduce(phones,
+            function(memo,phone){
+              if (phone.specs.hardware.physicalKeyboard)
+                memo.physical++;
+              else
+                memo.software++;
+              return memo;
+            },
+            { physical:0,software:0 });
+  appendToElement(root,"reduce - keyboard types",JSON.stringify(redResult));  
 
+  //filter + chaining + contains
+  var filterResult = _.chain(phones)
+                      .filter(function(phone){
+                        return _.contains(phone.specs.availability,"Verizon");
+                      }).map(function(phone){
+                        return "<p>"+phone.name+"</p>";
+                      }).reduce(function(memo,phoneTag){
+                        return memo + phoneTag;
+                      },"")
+                      .value();
+  // join makes more sense than that last reduce, but whatever
+  appendToElement(root,"filter + chaining - verizon phones",
+                  filterResult);  
+
+  //countby
+  var countResult = _.countBy(phones,function(phone){
+
+  });
+
+  //compose
 
 }
 
